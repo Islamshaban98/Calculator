2fd19e4b06c469f750831febb6a58bfbe0b374e4
Islamshaban98 <islamshaban998@gmail.com>
2020-10-17 19:19:48 +0300

    adding eslint config and design the calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="display">0</div>
+        <div className="buttons">
+          <Button content="AC" type="function" />
+          <Button content="+/-" type="function" />
+          <Button content="%" type="function" />
+          <Button content="÷" type="operator" />
+          <Button content="7" />
+          <Button content="8" />
+          <Button content="9" />
+          <Button content="×" type="operator" />
+          <Button content="4" />
+          <Button content="5" />
+          <Button content="6" />
+          <Button content="−" type="operator" />
+          <Button content="1" />
+          <Button content="2" />
+          <Button content="3" />
+          <Button content="+" type="operator" />
+          <Button content="0" />
+          <Button content="." />
+          <Button content="=" type="operator" />
+        </div>
+      </div>
+    );
+  }
 }
+// ± % ÷ × − + =
 
 export default App;
